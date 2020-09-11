bd9e1c0689a99bcbc529806fe80f37137de85806
Raggi Izar <61994369+Raggiiz@users.noreply.github.com>
2020-09-11 16:06:23 -0300

    metodo GET

--- employee-api/src/controllers/employee.controller.js
+++ employee-api/src/controllers/employee.controller.js
@@ -2,21 +2,25 @@
  * Arquivo responsável pela lógica do CRUD da API - Employee
  */
 
- const db = require('../config/database');
+const db = require('../config/database');
 
- // => Método responsável por criar um novo 'Employee'
-// ==> Método responsável por criar um novo 'Employee':
-exports.createEmployee = async(req, res) => {
-    const { name, job_role, salary, birth, employee_registration } = req.body;
-    const { rows } = await db.query(
-      "INSERT INTO employee (name, job_role, salary, birth, employee_registration) VALUES ($1, $2, $3, $4, $5)",
-        [name, job_role, salary, birth, employee_registration]
-    );
-  
-    res.status(201).send({
-      message: 'Employee added successfully!',
-      body: {
-        employee: { name, job_role, salary, birth, employee_registration }
-      },
-    });
-  };
+// => Método responsável por criar um novo 'Employee'
+exports.createEmployee = async (req, res) => {
+  const { name, job_role, salary, birth, employee_registration } = req.body;
+  const { rows } = await db.query(
+    "INSERT INTO employee (name, job_role, salary, birth, employee_registration) VALUES ($1, $2, $3, $4, $5)",
+    [name, job_role, salary, birth, employee_registration]
+  );
+
+  res.status(201).send({
+    message: 'Employee added successfully!',
+    body: {
+      employee: { name, job_role, salary, birth, employee_registration }
+    },
+  });
+};
+// => Método responsável por listar todos os 'employees'
+exports.listAllEmployees = async (req, res) => {
+  const response = await db.query('SELECT * FROM employee ORDER BY name ASC');
+  res.status(200).send(response.rows);
+};
--- employee-api/src/routes/employee.routes.js
+++ employee-api/src/routes/employee.routes.js
@@ -5,12 +5,12 @@
 const router = require('express-promise-router')();
 const employeeController = require('../controllers/employee.controller');
 
- // => Definindo as rotas do CRUD - 'Employee'
+// => Definindo as rotas do CRUD - 'Employee'
 
- // => Rota responsável por criar um novo colaborador : (POST): localhost://3000/api/employees
- router.post('/employees', employeeController.createEmployee);
+// => Rota responsável por criar um novo colaborador : (POST): localhost://3000/api/employees
+router.post('/employees', employeeController.createEmployee);
 
- // => Rota responsável por listar os colaboradores : (GET): localhost://3000/api/employees
-//  router.get('/employees', employeeController.listAllEmployees);
+//=> Rota responsável por listar os colaboradores : (GET): localhost://3000/api/employees
+router.get('/employees', employeeController.listAllEmployees);
 
- module.exports = router;
+module.exports = router;
