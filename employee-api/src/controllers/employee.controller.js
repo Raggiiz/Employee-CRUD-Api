97455bd70bb9bf0fc02ffdc03ea91a2bb137d2db
Raggi Izar <61994369+Raggiiz@users.noreply.github.com>
2020-09-11 16:57:37 -0300

    Update employee

--- employee-api/src/controllers/employee.controller.js
+++ employee-api/src/controllers/employee.controller.js
@@ -32,3 +32,15 @@ exports.FindEmployeeById = async(req, res) => {
   const response = await db.query('SELECT * FROM employee WHERE employee_id = $1', [employeeId]);
   res.status(200).send(response.rows);
 }
+
+// => Método responsável por atualizar um os 'employee' específico por ID
+exports.updateEmployeeById = async(req, res) => {
+  const employeeId = req.params.id;
+  const { name, job_role, salary, birth, employee_registration } = req.body;
+
+  const response = await db.query(
+  'UPDATE employee SET name = $1, job_role = $2, salary = $3, birth = $4, employee_registration = $5 WHERE employee_id = $6',
+  [name, job_role, salary, birth, employee_registration, employeeId]
+  );
+  res.status(200).send({ message: 'Employee updated successfully!' })
+}
--- employee-api/src/routes/employee.routes.js
+++ employee-api/src/routes/employee.routes.js
@@ -13,7 +13,10 @@ router.post('/employees', employeeController.createEmployee);
 //=> Rota responsável por listar os colaboradores : (GET): localhost://3000/api/employees
 router.get('/employees', employeeController.listAllEmployees);
 
-//=> Rota responsável por listar um colaborador específico : (GET): localhost://3000/api/employees
+//=> Rota responsável por listar um colaborador específico : (GET): localhost://3000/api/employees/:id
 router.get('/employees/:id', employeeController.FindEmployeeById);
+
+//=> Rota responsável por atualizar um colaborador específico : (GET): localhost://3000/api/employees/:id
+router.put('/employees/:id', employeeController.updateEmployeeById);
 
 module.exports = router;
