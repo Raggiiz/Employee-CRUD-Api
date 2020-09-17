d70e5b8bb741c408b29e43c109e4eb26bfea20ef
Raggi Izar <61994369+Raggiiz@users.noreply.github.com>
2020-09-17 16:24:05 -0300

    Update app-routing.module.ts

--- front-angular/src/app/app-routing.module.ts
+++ front-angular/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { EmployeeAddComponent } from './employee-add/employee-add.component';
+import { EmployeeEditComponent } from './employee-edit/employee-edit.component';
+import { EmployeeGetComponent } from './employee-get/employee-get.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'employee/create', component: EmployeeAddComponent },
+  { path: 'employee/:id', component: EmployeeEditComponent },
+  { path: 'employee', component: EmployeeGetComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
