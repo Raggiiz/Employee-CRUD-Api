acb273fcd6b30ed96ef1009698d6cf5540ac0826
Raggi Izar <61994369+Raggiiz@users.noreply.github.com>
2020-09-18 15:38:31 -0300

    loadingBar working

--- front-angular/src/app/app.component.ts
+++ front-angular/src/app/app.component.ts
@@ -11,4 +11,22 @@ import { NavigationCancel, Event, NavigationEnd,
 })
 export class AppComponent {
   title = 'front-angular';
+  constructor(private loadingBar: SlimLoadingBarService, private router: Router){
+    this.router.events.subscribe((event: Event)=>{
+      this.navigationInterceptor(event);
+    });
+    
+  }
+
+  private navigationInterceptor(event: Event): void {
+      if (event instanceof NavigationStart){
+        this.loadingBar.start();
+      }if(event instanceof NavigationEnd){
+        this.loadingBar.complete();
+      } if(event instanceof NavigationCancel){
+        this.loadingBar.stop();
+      } if(event instanceof NavigationError){
+        this.loadingBar.stop();
+      } 
+  }
 }
